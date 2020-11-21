21be25f1949e7386798f227830d39a84d0198c41
jruipinto <rui.jruipinto@gmail.com>
2020-11-21 13:42:15 +0000

    refactor: add youtube channel link
    add youtube channel link to "contactos
    section"

---
+++ src/app/components/youtube-icon/youtube-icon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'youtube-icon',
+  templateUrl: './youtube-icon.component.html',
+  styleUrls: ['./youtube-icon.component.css']
+})
+export class YoutubeIconComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
